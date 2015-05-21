e897343a787e8dcc68b0c3f272bb456ec9444c02
Nikolai Ivanov <mraiur@gmail.com>
2015-05-22 01:22:15 +0300

    Fix navigation for smaller screens.

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" type="text/css" media="screen, projection">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <style type="text/css">
             .clear{
@@ -12,12 +13,28 @@
     <body>
         <nav class="navbar navbar-default">
             <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse" id="navbar">
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.php">Check list</a></li>
-                        <li><a href="random.php">Random seats</a></li>
+                    <ul class="nav navbar-nav navbar-right" >
+                        <li>
+                            <a href="index.php">
+                                <i class="glyphicon glyphicon-ok"></i> Check list
+                            </a>
+                        </li>
+                        <li>
+                            <a href="random.php">
+                                <i class="glyphicon glyphicon-refresh"></i> Random seats
+                            </a>
+                        </li>
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
